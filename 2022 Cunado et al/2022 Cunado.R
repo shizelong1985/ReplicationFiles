60a83fecc8bfebea448c75b1df28e77097789f7d
Gabauer David <david.gabauer@hotmail.com>
2022-01-31 17:40:43 +0100

    add extended joint quantile connectedness

---
+++ 2022 Cunado et al/2022 Cunado.R
@@ -0,0 +1,42 @@
+
+### Dynamic spillovers across precious metals and energy realized volatilities: Evidence from quantile extended joint connectedness measures
+### CUNADO, J., CHATZIANTONIOU, GABAUER, D., GARCIA DE PEREZ, F., & HARDIK, M.
+### replicated by David Gabauer
+
+library("zoo")
+library("parallel")
+library("ConnectednessApproach")
+options(mc.cores=detectCores())
+
+data(ccggh2022)
+Y = ccggh2022[,c(1,2,5,6)]
+k = ncol(Y)
+NAMES = colnames(Y)
+
+par(mfcol = c(ceiling(k/2), 2), oma = c(1,1,0,0) + 0.1, mar = c(1,0.5,0.5,0) + 1, mgp=c(0, .65, 0))
+for (i in 1:k) {
+  plot(Y[,i],type="l",las=1,xlab="",ylab="",main=NAMES[i],ylim=c(0,1.5),col="steelblue4",xaxs="i")
+  grid(NA,NULL)
+  lines(Y[,i],col="steelblue4")
+  box()
+}
+print(SummaryStatistics(Y))
+
+# DYNAMIC RETURN CONNECTEDNESS
+tau = 0.50
+dca = ConnectednessApproach(Y, 
+                            model="QVAR",
+                            connectedness="Extended Joint",
+                            nlag=1,
+                            nfore=20,
+                            window.size=250,
+                            VAR_config=list(QVAR=list(tau=tau)))
+print(dca$TABLE)
+
+# CONNECTEDNESS PLOTS
+plot_tci(dca, save=TRUE)
+plot_net(dca, save=TRUE)
+plot_to(dca, save=TRUE)
+plot_from(dca, save=TRUE)
+plot_npso(dca, save=TRUE)
+plot_pci(dca, save=TRUE)
