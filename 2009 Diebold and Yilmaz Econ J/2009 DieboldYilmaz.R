54b7e26d2101d6e88e905c368f33e19235413532
Gabauer David <david.gabauer@hotmail.com>
2022-02-01 12:25:46 +0100

    Diebold and Yilmaz (2009)

--- 2009 Diebold and Yilmaz Econ J/2009 DieboldYilmaz.R
+++ 2009 Diebold and Yilmaz Econ J/2009 DieboldYilmaz.R
@@ -1,18 +1,23 @@
 
-### DIEBOLD, FX., AND YILMAZ, K. (2009)
-### MEASURING FINANCIAL ASSET RETURN AND VOLATILITY SPILLOVERS, WITH APPLICATION TO GLOBAL EQUITY MARKETS
+### Measuring financial asset return and volatility spillovers, with application to global equity markets
+### Diebold, FX., & Yilmaz, K. (2009)
 ### Economic Journal
 ### by David Gabauer
 
-options(mc.cores=detectCores())
+library("parallel")
 library("ConnectednessApproach")
+options(mc.cores=detectCores())
 data("dy2009")
 
-### STATIC CONNECTEDNESS
+### INITILISATION
 nlag = 2
 nfore = 10
+window.size = 200
+generalized = FALSE
+
+### STATIC CONNECTEDNESS
 fit = VAR(dy2009, configuration=list(nlag=nlag))
-DynamicConnectedness(Phi=fit$B, Sigma=fit$Q, nfore=nfore, generalized=FALSE)$TABLE
+DynamicConnectedness(Phi=fit$B, Sigma=fit$Q, nfore=nfore, generalized=generalized)$TABLE
 
 # DYNAMIC CONNECTEDNESS
 dca = ConnectednessApproach(dy2009, 
@@ -20,12 +25,13 @@ dca = ConnectednessApproach(dy2009,
                             connectedness="Time",
                             nlag=nlag,
                             nfore=nfore,
-                            window.size=200,
-                            Connectedness_config=list(TimeConnectedness=list(generalized=FALSE)))
+                            window.size=window.size,
+                            Connectedness_config=list(TimeConnectedness=list(generalized=generalized)))
+print(dca$TABLE)
 
 # CONNECTEDNESS PLOTS
-plot_tci(dca, save=TRUE)
-plot_net(dca, save=TRUE)
-plot_to(dca, save=TRUE)
-plot_from(dca, save=TRUE)
-plot_npso(dca, save=TRUE)
+plot_tci(dca)
+plot_net(dca)
+plot_to(dca)
+plot_from(dca)
+plot_npdc(dca)
