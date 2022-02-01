cfebf83b09e5872bafdff74798f37d77fa0df1de
Gabauer David <david.gabauer@hotmail.com>
2022-02-01 12:55:09 +0100

    Antonakakis et al. (2019)

--- 2019 Antonakakis et al. J. Int. Financial Mark. Inst. Money/2019 Antonakakis et al. J. Int. Financial Mark. Inst. Money.R
+++ 2019 Antonakakis et al. J. Int. Financial Mark. Inst. Money/2019 Antonakakis et al. J. Int. Financial Mark. Inst. Money.R
@@ -1,8 +1,8 @@
 
-### ANTONAKAKIS, N., CHATZIANTONIOU, I., AND GABAUER, D. (2019)
-### CRYPTOCURRENCY MARKET CONTAGION: MARKET UNCERTAINTY, MARKET COMPLEXITY, AND DYNAMIC PORTFOLIOS (I)
+### Cryptocurrency market contagion: Market uncertainty, market complexity, and dynamic portfolios
+### Antonakakis, N., Chatziantoniou, I., & Gabauer, D. (2019)
 ### Journal of International Financial Markets, Institutions and Money
-### by David Gabauer (https://sites.google.com/view/davidgabauer/contact-details)
+### by David Gabauer
 
 # Information: As my laptop had a defect in 2019 I have lost the original code of this paper.
 # Hence, this is an approximation of the original paper. Instead of the TVP-FAVAR method of Koop and Korobilis (2014)
@@ -13,6 +13,7 @@
 # Even though this is not the same it leads to similar results and I sincerely hope that it will be useful for future research.
 # Kind regards, David
 
+rm(list=c())
 library("zoo")
 library("psych")
 library("openxlsx")
@@ -21,20 +22,14 @@ library("ConnectednessApproach")
 options(mc.cores=detectCores())
 
 RAW = na.omit(read.xlsx('./data.xlsx', detectDates=TRUE))
-DATE = as.Date(RAW[,1])
+DATE = as.Date(RAW[,1], "%Y-%m-%d")
 DATA = RAW[,-1]
 DATA = zoo(DATA, order.by=DATE)
 k = ncol(DATA)
 NAMES = colnames(DATA)
 
-# DATA TRANSFORMATION
-X = DATA[-1,]
-for (i in 1:k) {
-  x = embed(as.numeric(DATA[,i]),2)
-  X[,i] = 100*(x[,1]-x[,2])/x[,1]
-}
-Y = X[,1:9]
-Z = X[,-c(1:9)]
+Y = DATA[,1:9]
+Z = DATA[,-c(1:9)]
 PC = principal(Z, nfactors=1)$scores
 Y = cbind(Y, PC)
 k = ncol(Y)
@@ -48,13 +43,13 @@ dca = ConnectednessApproach(Y,
                             window.size=200,
                             VAR_config=list(TVPVAR=list(kappa1=0.99, kappa2=0.99, prior="BayesPrior")))
 ind = which(index(Y)=="2017-07-20")
-ConnectednessTable(dca$FEVD[,,c(1:ind)])$TABLE
-ConnectednessTable(dca$FEVD[,,-c(1:ind)])$TABLE
+ConnectednessTable(dca$CT[,,c(1:ind)])$TABLE
+ConnectednessTable(dca$CT[,,-c(1:ind)])$TABLE
 
 # CONNECTEDNESS PLOTS
-plot_tci(dca, save=TRUE)
-plot_net(dca, save=TRUE)
-plot_to(dca, save=TRUE)
-plot_from(dca, save=TRUE)
-plot_npso(dca, save=TRUE)
-plot_pci(dca, save=TRUE)
+plot_tci(dca)
+plot_net(dca)
+plot_to(dca)
+plot_from(dca)
+plot_npso(dca)
+plot_pci(dca)
