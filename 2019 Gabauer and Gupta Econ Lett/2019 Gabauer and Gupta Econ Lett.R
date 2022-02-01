d44cfd8efb2b5a91b35a5ebba847370f34c5d592
Gabauer David <david.gabauer@hotmail.com>
2022-02-01 13:02:21 +0100

    Gabauer and Gupta (2019)

--- 2019 Gabauer and Gupta Econ Lett/2019 Gabauer and Gupta Econ Lett.R
+++ 2019 Gabauer and Gupta Econ Lett/2019 Gabauer and Gupta Econ Lett.R
@@ -1,16 +1,18 @@
 
 ### On The Transmission Mechanism Of Country-Specific And International Economic Uncertainty Spillovers:
 ### Evidence From A TVP-VAR Connectedness Decomposition Approach
-### GABAUER, D., and GUPTA, R. (2019)
-### ECONOMICS LETTERS
+### Gabauer, D., & Gupta, R. (2019)
+### Economics Letters
 ### replicated by David Gabauer
 
+rm(list=c())
+library("parallel")
 library("ConnectednessApproach")
 options(mc.cores=detectCores())
 
 # DYNAMIC CONNECTEDNESS
-data("gg2019")
-dca = ConnectednessApproach(gg2019, 
+data("gg2018")
+dca = ConnectednessApproach(gg2018, 
                             model="TVP-VAR",
                             connectedness="Time",
                             nlag=1,
@@ -18,6 +20,8 @@ dca = ConnectednessApproach(gg2019,
                             window.size=200,
                             VAR_config=list(TVPVAR=list(kappa1=0.99, kappa2=0.99, prior="BayesPrior")))
 print(dca$TABLE)
+
+# CONNECTEDNESS PLOTS
 plot_tci(dca)
 plot_net(dca)
 plot_to(dca)
@@ -25,10 +29,23 @@ plot_from(dca)
 plot_npso(dca)
 
 # CONNECTEDNESS DECOMPOSITION
-cd = ConnectednessDecomposition(dca, groups=list("US"=c(1,2,3,4), "JP"=c(5,6,7,8)), corrected=FALSE)
+cd = ConnectednessDecompose(dca, groups=list("US"=c(1,2,3,4), "JP"=c(5,6,7,8)))
 print(cd$TABLE)
 
+# CONNECTEDNESS PLOTS
+plot_tci(cd)
 plot_net(cd)
 plot_to(cd)
 plot_from(cd)
 plot_npso(cd)
+
+# AGGREGATED DECOMPOSITION
+ca = ConnectednessAggregate(dca, groups=list("US"=c(1,2,3,4), "JP"=c(5,6,7,8)))
+print(ca$TABLE)
+
+# CONNECTEDNESS PLOTS
+plot_tci(ca)
+plot_net(ca)
+plot_to(ca)
+plot_from(ca)
+plot_npso(ca)
