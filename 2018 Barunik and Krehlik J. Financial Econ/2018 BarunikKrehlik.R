c482d553953e6cdc3523693a02d890c4fc9f2939
Gabauer David <david.gabauer@hotmail.com>
2022-02-01 12:19:55 +0100

    Barunik and Krehlik (2018)

--- 2018 Barunik and Krehlik J. Financial Econ/2018 BarunikKrehlik.R
+++ 2018 Barunik and Krehlik J. Financial Econ/2018 BarunikKrehlik.R
@@ -1,25 +1,39 @@
 
-### BARUNIK AND KREHLIK (2018)
 ### Measuring the Frequency Dynamics of Financial Connectedness and Systemic Risk 
+### Barunik, J. & Krehlik, T. (2018)
 ### Journal of Financial Econometrics
 ### replicated by David Gabauer
 
-options(mc.cores=detectCores())
+rm(list=ls())
+library("parallel")
 library("ConnectednessApproach")
+options(mc.cores=detectCores())
 data("dy2012")
 
-# STATIC CONNECTEDNESS
+# INITIALISATION
 nlag = 2
 nfore = 100
 partition = c(pi+0.00001, pi/4, 0)
+
+# STATIC FREQUENCY CONNECTEDNESS
 fit = VAR(dy2012, configuration=list(nlag=nlag))
 FrequencyConnectedness(Phi=fit$B, Sigma=fit$Q, nfore=nfore,partition=partition, generalized=TRUE)$TABLE
 
-# DYNAMIC CONNECTEDNESS
+# DYNAMIC FREQUENCY CONNECTEDNESS
 dca = ConnectednessApproach(dy2012, model="VAR",
                             connectedness="Frequency",
                             nlag=nlag, 
                             nfore=nfore,
-                            Connectedness_config=list(FrequencyConnectedness=list(partition=partition, generalized=TRUE))
+                            window.size=200,
+                            Connectedness_config=list(FrequencyConnectedness=list(partition=partition, generalized=TRUE, scenario="ABS"))
 )
-dca$TABLE
+print(dca$TABLE)
+
+# CONNECTEDNESS PLOTS
+plot_tci(dca)
+plot_net(dca)
+plot_to(dca)
+plot_from(dca)
+plot_npdc(dca)
+plot_pci(dca)
+plot_influence(dca)
