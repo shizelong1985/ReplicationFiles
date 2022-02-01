97e2fb4335c49596ac9e1eaa01c2069f54f56a1e
Gabauer David <david.gabauer@hotmail.com>
2022-02-01 15:17:28 +0100

    Gabauer et al. (2022)

--- 2022 Gabauer et al/2022 Gabauer.R
+++ 2022 Gabauer et al/2022 Gabauer.R
@@ -1,6 +1,6 @@
 
 ### Quantile time-frequency price connectedness between green bond, green equity, sustainable investments and clean energy markets: Implications for eco-friendly investors
-### GABAUER, D., CHATZIANTONIOU, I., Aikins Abakah, E.J. & TIWARI, A.K.
+### Gabauer, D., Chatziantoniou, I., Aikins Abakah, EJ., & Tiwari, AK.
 ### replicated by David Gabauer
 
 library("zoo")
@@ -11,21 +11,18 @@ options(mc.cores=detectCores())
 
 data("gcat2022")
 DATA = gcat2022
-NAMES = c("Green Bond Index", 
-          "MSCI Global Environment",
-          "Sustainability Index World",
-          "Global Clean Energy")
-colnames(DATA) = NAMES
+NAMES = colnames(DATA)
 k = ncol(DATA)
 
 par(mfrow = c(1,1), oma = c(0.5,1,0,0) + 0.1, mar = c(0.5,1,1,0) + .5, mgp = c(3, 0.5, 0))
-plot(scale(DATA[,1],T,T), type="l",las=1,xaxs="i",col="steelblue4",main="",tck=-0.01,ylim=c(-5,5))
+plot(scale(DATA[,1],TRUE,TRUE), type="l",las=1,xaxs="i",col="steelblue4",main="",tck=-0.01,ylim=c(-5,5))
 grid(NA, NULL)
 for (i in k:1) {
-  lines(scale(DATA[,i],T,T), col=i, lty=i)
+  lines(scale(DATA[,i],TRUE,TRUE), col=i, lty=i)
 }
 legend("topleft", NAMES, fill=1:k, bty="n")
 
+# DATA TRANSFORMATION
 Y = DATA[-1,]
 for (i in 1:k) {
   Y[,i] = 100*diff(log(DATA[,i]))
@@ -52,19 +49,14 @@ dca = ConnectednessApproach(Y,
                             window.size=200,
                             VAR_config=list(QVAR=list(tau=0.5)),
                             Connectedness_config=list(
-                              FrequencyConnectedness=list(partition=partition, generalized=TRUE)
+                              FrequencyConnectedness=list(partition=partition, generalized=TRUE, scenario="ABS")
                             ))
 print(dca$TABLE)
 
-par(mfrow=c(1,1))
-plot(dca$TCI[,1,1]+dca$TCI[,2,1], type="l",ylim=c(0,80),xaxs="i",las=1)
-lines(dca$TCI[,1,1], col=2)
-lines(dca$TCI[,2,1], col=3)
-
-par(mfrow=c(2,2))
-for (i in 1:k) {
-  plot(dca$NET[,i,1,1], type="l",ylim=c(-20,20),xaxs="i",main=NAMES[i],las=1)
-  abline(h=0, lty=3)
-  lines(dca$NET[,i,2,1], col=2)
-  lines(dca$NET[,i,1,1]+dca$NET[,i,2,1], col=3)
-}
+# CONNECTEDNESS PLOTS
+plot_tci(dca)
+plot_net(dca)
+plot_to(dca)
+plot_from(dca)
+plot_pci(dca)
+plot_npso(dca)
