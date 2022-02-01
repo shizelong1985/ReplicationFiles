ad3a0022804a3fdbc3b58346ac626bfba3a11c04
Gabauer David <david.gabauer@hotmail.com>
2022-02-01 12:30:19 +0100

    Diebold and Yilmaz (2012)

--- 2012 Diebold and Yilmaz Int. J. Forecast/2012 DieboldYilmaz.R
+++ 2012 Diebold and Yilmaz Int. J. Forecast/2012 DieboldYilmaz.R
@@ -1,16 +1,24 @@
-### DIEBOLD, FX., AND YILMAZ, K. (2012)
-### BETTER TO GIVE THAN TO RECEIVE: PREDICTIVE DIRECTIONAL MEASUREMENT OF VOLATILITY SPILLOVERS
+
+### Better to give than to receive: Predictive directional measurement of volatility spillovers
+### Diebold, FX., & Yilmaz, K. (2012)
 ### International Journal of Forecasting
 ### replicated by David Gabauer
 
+rm(list=c())
+library("parallel")
 library("ConnectednessApproach")
+options(mc.cores=detectCores())
 data("dy2012")
 
-# STATIC CONNECTEDNESS
+### INITILISATION
 nlag = 4
 nfore = 10
+window.size = 200
+generalized = TRUE
+
+# STATIC CONNECTEDNESS
 fit = VAR(dy2012, configuration=list(nlag=nlag))
-DynamicConnectedness(Phi=fit$B, Sigma=fit$Q, nfore=nfore, generalized=TRUE)$TABLE
+DynamicConnectedness(Phi=fit$B, Sigma=fit$Q, nfore=nfore, generalized=generalized)$TABLE
 
 # DYNAMIC CONNECTEDNESS
 dca = ConnectednessApproach(dy2012, 
@@ -18,12 +26,16 @@ dca = ConnectednessApproach(dy2012,
                             connectedness="Time",
                             nlag=nlag,
                             nfore=nfore,
-                            window.size=200,
-                            Connectedness_config=list(TimeConnectedness=list(generalized=TRUE)))
+                            window.size=window.size,
+                            Connectedness_config=list(TimeConnectedness=list(generalized=generalized)))
+print(dca$TABLE)
 
 # CONNECTEDNESS PLOTS
-plot_tci(dca, save=TRUE)
-plot_net(dca, save=TRUE)
-plot_to(dca, save=TRUE)
-plot_from(dca, save=TRUE)
-plot_npso(dca, save=TRUE)
+plot_tci(dca)
+plot_net(dca)
+plot_to(dca)
+plot_from(dca)
+plot_npdc(dca)
+plot_pci(dca)
+plot_npso(dca)
+plot_influence(dca)
